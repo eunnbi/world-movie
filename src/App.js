29093da82229b987e4a9dfb6d111d73e96871655
Eunbi Kang <jenabill@naver.com>
2022-06-16 18:44:59 +0900

    Refactor: ThemeProvider 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
-import { MoviesProvider } from './contexts/movies';
-import { FavoritesProvider } from './contexts/favorites';
-import Header from './components/Header';
-import Loading from './components/common/Loading';
-import loadable from '@loadable/component';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import { MoviesProvider } from "./contexts/movies";
+import { FavoritesProvider } from "./contexts/favorites";
+import Header from "./components/Header";
+import Loading from "./components/common/Loading";
+import loadable from "@loadable/component";
+import { QueryClient, QueryClientProvider } from "react-query";
+import theme from "./styles/theme";
+import { ThemeProvider } from "styled-components";
 const Home = loadable(() => import("./pages/Home"));
 const Search = loadable(() => import("./pages/Search"));
 const Details = loadable(() => import("./pages/Details"));
@@ -19,21 +21,23 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <MoviesProvider>
         <FavoritesProvider>
-          <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Suspense fallback={<Loading/>}>
-              <Routes>
-                <Route element={<Header/>}>
-                  <Route path="/" element={<Home/>}/>
-                  <Route path="/search" element={<Search/>}/>
-                  <Route path="/movies/:type" element={<Movies/>}/>
-                  <Route path="/favorites" element={<Favorites/>}/>
-                </Route>
-                <Route path="/movie/:id" element={<Details/>}/>
-              </Routes>
-            </Suspense>
-          </BrowserRouter>  
+          <ThemeProvider theme={theme}>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+              <Suspense fallback={<Loading />}>
+                <Routes>
+                  <Route element={<Header />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/search" element={<Search />} />
+                    <Route path="/movies/:type" element={<Movies />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                  </Route>
+                  <Route path="/movie/:id" element={<Details />} />
+                </Routes>
+              </Suspense>
+            </BrowserRouter>
+          </ThemeProvider>
         </FavoritesProvider>
-      </MoviesProvider>  
+      </MoviesProvider>
     </QueryClientProvider>
   );
 }
