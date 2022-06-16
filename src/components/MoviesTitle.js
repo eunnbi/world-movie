25f2bc39b4306deb15ffdc6555ec01bbdfd5212e
Eunbi Kang <jenabill@naver.com>
2022-06-16 20:22:32 +0900

    MoviesTitle 컴포넌트 css 단위 px에서 rem으로 수정

--- src/components/MoviesTitle.js
+++ src/components/MoviesTitle.js
@@ -1,25 +1,25 @@
 import Heading from "./common/Heading";
-import {BiCameraMovie} from "react-icons/bi";
+import { BiCameraMovie } from "react-icons/bi";
 import styled from "styled-components";
 
 const StyledMoviesTitle = styled.div`
-    margin-bottom: 50px;
-    text-transform: capitalize;
-    font-size: 40px;
-    display: flex;
-    align-items: center;
-    .MoviesTitle-icon {
-        margin-left: 20px;
-        color: #073763;
-    }
-`
-function MoviesTitle({type}){
-    return (
-        <StyledMoviesTitle className="movies__title">
-            <Heading size={40}>{type.replace("_", " ")} movies</Heading>
-            <BiCameraMovie className="MoviesTitle-icon"/>
-        </StyledMoviesTitle>
-    )
+  margin-bottom: 3rem;
+  text-transform: capitalize;
+  display: flex;
+  align-items: center;
+  .MoviesTitle-icon {
+    font-size: 2.5rem;
+    margin-left: 1rem;
+    color: #073763;
+  }
+`;
+function MoviesTitle({ type }) {
+  return (
+    <StyledMoviesTitle className="movies__title">
+      <Heading>{type.replace("_", " ")} movies</Heading>
+      <BiCameraMovie className="MoviesTitle-icon" />
+    </StyledMoviesTitle>
+  );
 }
 
 export default MoviesTitle;
