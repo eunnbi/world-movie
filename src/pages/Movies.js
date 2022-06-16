76934459adfa89ca42586f53b533d529c9189059
Eunbi Kang <jenabill@naver.com>
2022-06-16 20:53:25 +0900

    Design: 불필요한 스타일 속성들 삭제

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,43 +1,42 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import MoviesTitle from "../components/MoviesTitle";
-import MovieList from "../components/MovieList";
+import MovieList from "../components/common/MovieList";
 import Pagination from "../components/Pagination";
 import { moviesContext } from "../contexts/movies";
 import { useScroll } from "../hooks";
 import styled from "styled-components";
 
-
-
 const MoviesMain = styled.main`
-    width: 90vw;
-    max-width: 1170px;
-    margin: 110px auto;
-    padding: 3rem 0;
+  width: 90vw;
+  padding: 2.5rem 0;
 `;
 
 const Movies = () => {
-    const { type } = useParams();
-    const { movies } = useContext(moviesContext);
-    const data = movies[type];
-    const [page, setPage] = useState(1);
-    const limit = 12;
-    const offset = (page - 1) * limit;
-    useScroll([page]);
+  const { type } = useParams();
+  const { movies } = useContext(moviesContext);
+  const data = movies[type];
+  const [page, setPage] = useState(1);
+  const limit = 12;
+  const offset = (page - 1) * limit;
+  useScroll([page]);
 
-    return (
-        <MoviesMain className="movies__main">
-            {type && 
-                <MoviesTitle type={type}/>
-            }
-            {data &&
-                <>
-                    <MovieList movies={data.slice(offset, offset + limit)}/>
-                    <Pagination total={data.length} limit={limit} page={page} setPage={setPage}/>
-                </>
-            }
-        </MoviesMain>
-    );
-}
+  return (
+    <MoviesMain>
+      {type && <MoviesTitle type={type} />}
+      {data && (
+        <>
+          <MovieList movies={data.slice(offset, offset + limit)} />
+          <Pagination
+            total={data.length}
+            limit={limit}
+            page={page}
+            setPage={setPage}
+          />
+        </>
+      )}
+    </MoviesMain>
+  );
+};
 
 export default Movies;
