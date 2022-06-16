25cc44fd444611ac7fdae0326764ffd8361e5ad7
Eunbi Kang <jenabill@naver.com>
2022-06-16 21:13:39 +0900

    Refactor: 스타일 컴포넌트 분리

--- src/components/Movie/MovieInfo.js
+++ src/components/Movie/MovieInfo.js
@@ -1,56 +1,62 @@
-import {AiFillStar} from "react-icons/ai";
+import { AiFillStar } from "react-icons/ai";
 import Heading from "../common/Heading";
 import styled from "styled-components";
 
 const StyledMovieInfo = styled.div`
-    visibility: hidden;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    color: #fff;
-    padding: 0.5rem 1rem;
-    transition: all 0.3s linear;
-    transform: translateY(100%);
-    .MovieInfo-row:last-child {
-      display: flex;
-      justify-content: space-between;
-    }
-    .MovieInfo-rating {
-        display: flex;
-        align-items: center;
-        .icon {
-        color: #f4c519;
-        margin-right: 3px;
-        }
-    }
-    .MovieInfo-year {
-        font-size: 13px;
-    }
-    h2, h3{
-        margin-bottom: 5px;
-    }
+  visibility: hidden;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  padding: 0.5rem 1rem;
+  transition: all 0.3s linear;
+  transform: translateY(100%);
 `;
 
-function MovieInfo({title, originalTitle, year, rating}){
-    return (
-        <StyledMovieInfo className="MovieInfo">
-            <div className="MovieInfo-row">
-                <Heading level={2} size={15}>{title}</Heading>
-                {title !== originalTitle && <Heading level={3} size={13}>({originalTitle})</Heading>}
-            </div>
-            <div className="MovieInfo-row">
-                <p className="MovieInfo-year">{!isNaN(year) && year}</p>
-                {rating !== 0 &&
-                    <p className=" MovieInfo-rating">
-                        <AiFillStar className="icon"/>
-                        <span>{rating}</span>
-                    </p>
-                }  
-            </div>
-        </StyledMovieInfo>
-    );
+const MovieInfoRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .year {
+    font-size: 13px;
+  }
+`;
+
+const Rating = styled.p`
+  display: flex;
+  align-items: center;
+  svg {
+    color: #f4c519;
+    margin-right: 3px;
+  }
+`;
+
+function MovieInfo({ title, originalTitle, year, rating, ...props }) {
+  return (
+    <StyledMovieInfo {...props}>
+      <div>
+        <Heading level={4}>
+          {title}
+          {title !== originalTitle && (
+            <>
+              <br />({originalTitle})
+            </>
+          )}
+        </Heading>
+      </div>
+      <MovieInfoRow>
+        <p className="year">{!isNaN(year) && year}</p>
+        {rating !== 0 && (
+          <Rating>
+            <AiFillStar />
+            <span>{rating}</span>
+          </Rating>
+        )}
+      </MovieInfoRow>
+    </StyledMovieInfo>
+  );
 }
 
 export default MovieInfo;
