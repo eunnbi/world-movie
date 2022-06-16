3bfde01ce24e711b1d1dabe5acfbda06c2e9d2bc
Eunbi Kang <jenabill@naver.com>
2022-06-16 21:02:58 +0900

    Design: Movie 컴포넌트 스타일 속성 추가

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-import LikeHeart from "./LikeHeart";
+import LikeButton from "./LikeButton";
 import MoviePoster from "./MoviePoster";
 import MovieInfo from "./MovieInfo";
 import { favoritesStateContext } from "../../contexts/favorites";
@@ -8,32 +8,46 @@ import { useLike } from "../../hooks/useLike";
 import styled from "styled-components";
 
 const MovieLink = styled(Link)`
-    display: block;
-    position: relative;
-    overflow: hidden;
-    &:hover .MovieInfo {
-        visibility: visible;
-        transform: translateY(0);
-    }
+  display: block;
+  position: relative;
+  overflow: hidden;
+  height: 100%;
+  &:hover .MovieInfo {
+    visibility: visible;
+    transform: translateY(0);
+  }
 `;
 
-const Movie = ({movie}) => {
-    const { title, original_title : originalTitle, poster_path : poster, id, vote_average: rating} = movie;
-    const favoriteMovies = useContext(favoritesStateContext);
-    const { like, setLike, onLike } = useLike(false, movie);
+const Movie = ({ movie }) => {
+  const {
+    title,
+    original_title: originalTitle,
+    poster_path: poster,
+    id,
+    vote_average: rating,
+  } = movie;
+  const favoriteMovies = useContext(favoritesStateContext);
+  const { like, setLike, onLike } = useLike(false, movie);
 
-    useEffect(() => {
-        favoriteMovies.forEach(movie => movie.id === id && setLike(true));
-    }, []);
-    return (
-        <li className="Movie">
-            <MovieLink to={`/movie/${id}`}>
-                <LikeHeart like={like} onLike={onLike}/>
-                <MoviePoster poster={poster}/>
-                <MovieInfo title={title} originalTitle={originalTitle} year={parseInt(movie.release_date)} rating={rating}/>
-            </MovieLink>
-        </li>
-    );
-}
+  useEffect(() => {
+    favoriteMovies.forEach((movie) => movie.id === id && setLike(true));
+  }, []);
+
+  return (
+    <li>
+      <MovieLink to={`/movie/${id}`}>
+        <LikeButton like={like} onLike={onLike} />
+        <MoviePoster poster={poster} />
+        <MovieInfo
+          title={title}
+          originalTitle={originalTitle}
+          year={parseInt(movie.release_date)}
+          rating={rating}
+          className="MovieInfo"
+        />
+      </MovieLink>
+    </li>
+  );
+};
 
 export default Movie;
