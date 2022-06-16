ad700ef669be526f2b49154df22727620b2ad01d
Eunbi Kang <jenabill@naver.com>
2022-06-16 23:43:20 +0900

    Design: Loading 컴포넌트 스타일 수정

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,42 +1,42 @@
 import styled from "styled-components";
 
-const LoadingMain = styled.main`
-    position: absolute;
-    height: 100vh;
-    top: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: -1;
+const LoadingWrapper = styled.div`
+  position: fixed;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const StyledLoading = styled.div`
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    border: 10px solid #fff;
-    border-top: 10px solid #49a6e9;
-    animation: loading 1s linear infinite;
+const Circle = styled.div`
+  max-width: 100px;
+  max-height: 100px;
+  width: 30vw;
+  height: 30vw;
+  border-radius: 50%;
+  border: 10px solid #fff;
+  border-top: 10px solid ${({ theme }) => theme.color.skyBlue};
+  animation: loading 1s linear infinite;
 
-    @keyframes loading {
-        0% {
-            transform: rotateZ(0deg);
-        }
-        100% {
-            transform: rotateZ(360deg);
-        }
+  @keyframes loading {
+    0% {
+      transform: rotateZ(0deg);
     }
+    100% {
+      transform: rotateZ(360deg);
+    }
+  }
 `;
 
 const Loading = () => {
-    return(
-        <LoadingMain>
-           <StyledLoading></StyledLoading> 
-        </LoadingMain>
-        
-    );
-}
+  return (
+    <LoadingWrapper>
+      <Circle></Circle>
+    </LoadingWrapper>
+  );
+};
 
 export default Loading;
