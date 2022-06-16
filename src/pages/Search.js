95c6387eca9edffaaba8168840cc07d3d6454c87
Eunbi Kang <jenabill@naver.com>
2022-06-16 21:07:22 +0900

    Refactor and Design: 스타일 컴포넌트 분리 및 불필요한 스타일 속성 삭제

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,49 +1,47 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 import Loading from "../components/common/Loading";
-import MovieList from "../components/MovieList";
+import MovieList from "../components/common/MovieList";
 import SearchForm from "../components/SearchForm";
 import { searchMovies } from "../lib/api";
 import { useCloseNav } from "../hooks";
 import styled from "styled-components";
 
-
 const SearchMain = styled.main`
-    width: 90vw;
-    max-width: 1170px;
-    margin: 110px auto;
-    padding: 3rem 0;
-    .Search-not-found {
-        margin-top: 5px;
-        color: #bb2524;
-        font-size: 15px;
-        text-transform: capitalize;
-        letter-spacing: 2px;
-    }
-`
-function Search() {
-    useCloseNav();
-    const [query, setQuery] = useState("");
-    const {isLoading, data} = useQuery(["search", query], searchMovies);
+  padding: 3rem 0;
+`;
 
-    const onSearch = (e) => {
-        setQuery(e.target.value);
-    }
-    
-    return (
-        <SearchMain>
-            <SearchForm query={query} onSearch={onSearch}/>
-            {isLoading ? <Loading/> :
-                (query !== "" && data.length === 0) ? (
-                    <p className="Search-not-found">Movie Not Found!</p>
-                ) : (
-                    <section>
-                        <MovieList movies={data}/>
-                    </section> 
-                )  
-            }
-        </SearchMain>
-    );
+const NotFound = styled.p`
+  margin-top: 5px;
+  color: #bb2524;
+  font-size: 15px;
+  text-transform: capitalize;
+  letter-spacing: 2px;
+`;
+
+function Search() {
+  useCloseNav();
+  const [query, setQuery] = useState("");
+  const { isLoading, data } = useQuery(["search", query], searchMovies);
+
+  const onSearch = (e) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <SearchMain>
+      <SearchForm query={query} onSearch={onSearch} />
+      {isLoading ? (
+        <Loading />
+      ) : query !== "" && data.length === 0 ? (
+        <NotFound>Movie Not Found!</NotFound>
+      ) : (
+        <section>
+          <MovieList movies={data} />
+        </section>
+      )}
+    </SearchMain>
+  );
 }
 
 export default Search;
