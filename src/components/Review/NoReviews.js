4c295e5bc9c46012f75ac1b5f70a2b0b81eee45f
Eunbi Kang <jenabill@naver.com>
2022-06-16 21:47:38 +0900

    Design: NoReviews의 스타일 컴포넌트 수정

--- src/components/Review/NoReviews.js
+++ src/components/Review/NoReviews.js
@@ -1,21 +1,23 @@
-import {ImCrying} from "react-icons/im";
+import { ImCrying } from "react-icons/im";
 import styled from "styled-components";
 
 const StyledNoReviews = styled.p`
-    font-size: 30px;
-    color: #112a42;
-    text-transform: capitalize;
-    display: flex;
-    align-items: center;
-    .icon {
-        margin: 3px 0 0 10px;
-    }
+  font-size: 1.8rem;
+  color: ${({ theme }) => theme.color.blueBlack};
+  text-transform: capitalize;
+  display: flex;
+  align-items: center;
+  .icon {
+    margin: 3px 0 0 10px;
+  }
 `;
 
-function NoReviews(){
-    return (
-        <StyledNoReviews>no reviews <ImCrying className="icon"/></StyledNoReviews>
-    );
+function NoReviews() {
+  return (
+    <StyledNoReviews>
+      no reviews <ImCrying className="icon" />
+    </StyledNoReviews>
+  );
 }
 
 export default NoReviews;
