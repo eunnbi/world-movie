3d53a093a603681d49dbb9e3e283a11326040e24
Eunbi Kang <jenabill@naver.com>
2022-06-16 20:21:46 +0900

    Design: HomeMovieList 컴포넌트 불필요한 스타일 속성 삭제

--- src/components/HomeSection/HomeMovieList.js
+++ src/components/HomeSection/HomeMovieList.js
@@ -2,53 +2,39 @@ import Movie from "../Movie";
 import styled from "styled-components";
 
 const StyledHomeMoviesList = styled.ul`
-    display: flex;
-    overflow-x: scroll;
-    padding-bottom: 30px;
-    .Movie {
-        margin-right: 10px;
+  display: flex;
+  gap: 1rem;
+  overflow-x: scroll;
+  padding-bottom: 30px;
+  &::-webkit-scrollbar {
+    width: 6px;
+    height: 10px;
+    margin-top: 10px;
+    display: block;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.3);
+    border-radius: 5px;
+    transition: all 0.3s ease-in-out;
+    margin-top: 10px;
+    &:active {
+      background-color: rgba(0, 0, 0, 0.8);
     }
-    .Movie:last-child {
-        margin: 0;
-    }
-    &::-webkit-scrollbar {
-        width: 6px;
-        height: 10px;
-        margin-top: 10px;
-        display: block;
-    }
-    &::-webkit-scrollbar-thumb {
-        background-color: rgba(0, 0, 0, 0.3);
-        border-radius: 5px;
-        transition: all 0.3s ease-in-out;
-        margin-top: 10px;
-        &:active {
-            background-color: rgba(0, 0, 0, 0.8);
-        }
-    }
-    &::-webkit-scrollbar-track {
+  }
+  &::-webkit-scrollbar-track {
     background-color: transparent;
     margin-top: 10px;
-    }
-
-    @media screen and (max-width: 1000px){
-        .MoviePoster{
-            height: 300px;
-        }
-    }
+  }
 `;
 
-function HomeMovieList({movies}){
-    return (
-        <StyledHomeMoviesList>
-            {movies.map((movie, index) =>  (
-                <Movie
-                    key={index}
-                    movie={movie}
-                />
-            ))}
-        </StyledHomeMoviesList>
-    );
+function HomeMovieList({ movies }) {
+  return (
+    <StyledHomeMoviesList>
+      {movies.map((movie, index) => (
+        <Movie key={index} movie={movie} />
+      ))}
+    </StyledHomeMoviesList>
+  );
 }
 
 export default HomeMovieList;
