c34ed5f68d16c5cbf82276cbbf817820372d8ae1
Eunbi Kang <jenabill@naver.com>
2022-06-16 20:18:10 +0900

    Design: Heading 컴포넌트 폰트 사이즈 단위 rem으로 수정

--- src/components/common/Heading.js
+++ src/components/common/Heading.js
@@ -1,27 +1,23 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-const StyledHeading = styled.div.attrs(({level}) => ({
-    as: `h${level}`
+const StyledHeading = styled.div.attrs(({ level }) => ({
+  as: `h${level}`,
 }))`
-    font-weight: bold;
-    color: ${({fontcolor}) => fontcolor};
-    ${({level, size}) => size ? css`
-        font-size: ${size}px;
-    ` :css`
-        font-size: ${11 * (5 - level)}px;
-    `}
-`
+  font-weight: bold;
+  color: ${({ fontcolor }) => fontcolor};
+  font-size: ${({ level }) => `${(7 - level) / 3}rem`};
+`;
 
-function Heading({level, size, children, ...rest}){
-    return (
-        <StyledHeading level={level} size={size} {...rest}>
-            {children}
-        </StyledHeading>
-    );
+function Heading({ level, children, ...rest }) {
+  return (
+    <StyledHeading level={level} {...rest}>
+      {children}
+    </StyledHeading>
+  );
 }
 
 Heading.defaultProps = {
-    level: 1
+  level: 1,
 };
 
 export default Heading;
