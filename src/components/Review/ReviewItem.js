a903cec4babb5e14825df36a25e75c7d3a7cfe8f
Eunbi Kang <jenabill@naver.com>
2022-06-16 22:06:23 +0900

    Refactor and Design: 스타일 컴포넌트 분리 및 px에서 rem으로 단위 수정

--- src/components/Review/ReviewItem.js
+++ src/components/Review/ReviewItem.js
@@ -1,61 +1,63 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useToggle } from "../../hooks";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledReviewItem = styled.li`
-    background-color: #fff;
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
-    margin-bottom: 40px;
-    border-radius: 5px;
-    padding: 10px;
-    .ReviewItem-content {
-        color: #324d67;
-        line-height: 1.5;
-        a {
-            text-decoration: underline;
-        }
-        &.close {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 3;
-        white-space: normal;
-        -webkit-box-orient: vertical;
-        }
-    }
-    .ReviewItem-toggle {
-        color: #49a6e9;
-        text-transform: capitalize;
-        cursor: pointer;
-    }
-    .ReviewItem-username {
-        text-align: right;
-    }
-`
+  background-color: #fff;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 2.5rem;
+  border-radius: 5px;
+  padding: 0.9rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  .username {
+    text-align: right;
+  }
+`;
 
-function ReviewItem ({text, username}){
-    const [open, setOpen] = useToggle(false);
-    const [show, setShow] = useState(false);
-    const ref = useRef();
+const Content = styled.p`
+  opacity: 0.9;
+  line-height: 1.5;
+  ${({ close }) =>
+    close &&
+    css`
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      white-space: normal;
+      -webkit-box-orient: vertical;
+    `}
+`;
+const Toggle = styled.p`
+  color: ${({ theme }) => theme.color.skyBlue};
+  text-transform: capitalize;
+  cursor: pointer;
+`;
 
-    useEffect(() => {
-        ref.current.innerHTML = text;
-        if (ref.current.scrollHeight > 72){
-            setShow(true);
-        }
-    }, []);
+function ReviewItem({ text, username }) {
+  const [open, setOpen] = useToggle(false);
+  const [show, setShow] = useState(false);
+  const ref = useRef();
 
-    return (
-        <StyledReviewItem className="review">
-            <p className={open ? "ReviewItem-content" : "ReviewItem-content close"} ref={ref}></p>
-            {
-                show && 
-                <p className="ReviewItem-toggle" onClick={() => setOpen()}>{open ? "show less" : "read more"}</p>
-            }
-            
-            <p className="ReviewItem-username">{username}</p>
-        </StyledReviewItem>
-    );
+  useEffect(() => {
+    if (ref.current.scrollHeight > 72) {
+      setShow(true);
+    }
+  }, []);
+
+  return (
+    <StyledReviewItem>
+      <Content close={!open} ref={ref}>
+        {text}
+      </Content>
+      {show && (
+        <Toggle onClick={setOpen}>{open ? "show less" : "read more"}</Toggle>
+      )}
+      <p className="username">{username}</p>
+    </StyledReviewItem>
+  );
 }
 
 export default ReviewItem;
