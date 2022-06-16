c8ec5c320f8670d41b32fdd9741c84d16c8a1d77
Eunbi Kang <jenabill@naver.com>
2022-06-16 21:06:30 +0900

    Refactor and Design: 스타일 컴포넌트 분리 및 theme 객체 적용

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -3,60 +3,52 @@ import { NAV } from "../../constant/nav";
 import styled, { css } from "styled-components";
 
 const StyledNav = styled.nav`
-    background-color: #fff;
-    display: flex;
-    align-items: center;
-    border-radius: 10px;
-    a {
-        margin: 0 30px;
-        display: block;
-        font-size: 20px;
-        color: #073763;
-        font-weight: bold;
-        font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-        transition: all 0.3s ease-in-out;
-        &:hover {
-        color: #49a6e9;
-        }
-        &.active {
-            color: #49a6e9;
-        }
-    }
-    @media screen and (max-width: 900px) {
-        display: block;
-        width: 100%;
-        overflow: hidden;
-        height: 0px;
-        transition: all 0.3s linear;
-        border-radius: 0px;
-        ${({showNav}) => showNav && css`
-            height: 268px;
-        `}
-        a {
-            margin: 0;
-            padding: 20px 30px;
-            display: block;
-            font-size: 20px;
-            color: #073763;
-            font-weight: bold;
-            font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-            transition: all 0.3s ease-in-out;
-            &:hover {
-                background: #93d2ff;
-                color: #fff;
-            }
-        }
-    }
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  @media ${({ theme }) => theme.device.laptop} {
+    display: block;
+    width: 100%;
+    overflow: hidden;
+    height: ${({ showNav }) => (showNav ? "14rem" : "0px")};
+    transition: all 0.3s linear;
+  }
 `;
 
-function Nav({showNav}){
-    return (
-        <StyledNav showNav={showNav}>
-            {NAV.map(nav =>
-                <NavLink key={nav.id} to={nav.to} className={({isActive}) => isActive ? "active" : ""}>{nav.text}</NavLink>
-            )}
-        </StyledNav> 
-    );
+const Anchor = styled(NavLink)`
+  margin: 0 2rem;
+  display: block;
+  font-size: 1.2rem;
+  color: #073763;
+  font-weight: bold;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    color: #49a6e9;
+  }
+  &.active {
+    color: #49a6e9;
+  }
+  @media ${({ theme }) => theme.device.laptop} {
+    margin: 0;
+    padding: 1.5rem;
+    &:hover {
+      background: #93d2ff;
+      color: #fff;
+    }
+  }
+`;
+
+function Nav({ showNav }) {
+  return (
+    <StyledNav showNav={showNav}>
+      {NAV.map((nav) => (
+        <Anchor key={nav.id} to={nav.to}>
+          {nav.text}
+        </Anchor>
+      ))}
+    </StyledNav>
+  );
 }
 
 export default Nav;
