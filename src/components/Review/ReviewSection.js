e61ce7274a4a561372ad063903c45fac8710c429
Eunbi Kang <jenabill@naver.com>
2022-06-16 21:54:08 +0900

    Refactor: ReviewSection 컴포넌트의 스타일 컴포넌트 수정

--- src/components/Review/ReviewSection.js
+++ src/components/Review/ReviewSection.js
@@ -1,37 +1,33 @@
 import Heading from "../common/Heading";
-import { MdOutlineRateReview } from "react-icons/md";
-import styled from "styled-components";
 import NoReviews from "./NoReviews";
 import ReviewList from "./ReviewList";
+import { MdOutlineRateReview } from "react-icons/md";
+import styled from "styled-components";
 
-const StyledReviewSection = styled.section`
-    grid-column: 1/3;
-    .ReviewSectoin-title{
-        color: #112a42;
-        display: flex;
-        align-items: center;
-        font-size: 30px;
-        letter-spacing: 1px;
-        margin-bottom: 2rem;
-        border-bottom: 1px solid #55595c;
-        padding-bottom: 10px;
-        .icon {
-            margin: 5px 0 0 8px;
-        }
-    }
-`
+const ReviewHeading = styled.div`
+  color: ${({ theme }) => theme.color.blueBlack};
+  display: flex;
+  align-items: center;
+  font-size: 1.8rem;
+  letter-spacing: 1px;
+  margin-bottom: 2rem;
+  border-bottom: 1px solid ${({ theme }) => theme.color.gray};
+  padding-bottom: 0.6rem;
+  svg {
+    margin: 5px 0 0 8px;
+  }
+`;
 
-
-function ReviewSection({reviews}){
-    return (
-        <StyledReviewSection>
-                <div className="ReviewSectoin-title">
-                    <Heading level={1}>Reviews</Heading>
-                    <MdOutlineRateReview className="icon"/>
-                </div>
-                {reviews.length === 0 ? <NoReviews/> : <ReviewList reviews={reviews}/>}
-        </StyledReviewSection>
-    )
+function ReviewSection({ reviews }) {
+  return (
+    <article>
+      <ReviewHeading>
+        <Heading>Reviews</Heading>
+        <MdOutlineRateReview />
+      </ReviewHeading>
+      {reviews.length === 0 ? <NoReviews /> : <ReviewList reviews={reviews} />}
+    </article>
+  );
 }
 
 export default ReviewSection;
