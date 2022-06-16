714a15e67241e581a3733358ceb27c261710c40a
Eunbi Kang <jenabill@naver.com>
2022-06-16 21:42:35 +0900

    Design: Genres 컴포넌트 스타일 속성 수정

--- src/components/Details/Genres.js
+++ src/components/Details/Genres.js
@@ -1,34 +1,28 @@
 import styled from "styled-components";
 
 const GenresList = styled.ul`
-    display: flex;
-    margin-bottom: 1.8rem;
-    flex-wrap: wrap;
+  display: flex;
+  margin-bottom: 1.8rem;
+  flex-wrap: wrap;
+  gap: 0.5rem;
 `;
 
 const GenresItem = styled.li`
-    margin: 0 5px 5px 0;
-    background-color: #fff;
-    color: #55595c;
-    padding: 5px 10px;
-    border-radius: 5px;
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
-    font-weight: 400;
-    &:first-child {
-        margin-left: 0;
-    }
-`
+  background-color: #fff;
+  color: #55595c;
+  padding: 0.3rem 0.6rem;
+  border-radius: 5px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
+`;
 
-function Genres({genres}) {
-    return (
-        <GenresList>
-            {genres.map((genre, index) => 
-                <GenresItem key={index}>
-                    {genre.name}
-                </GenresItem>
-            )}
-        </GenresList>
-    );
+function Genres({ genres }) {
+  return (
+    <GenresList>
+      {genres.map((genre, index) => (
+        <GenresItem key={index}>{genre.name}</GenresItem>
+      ))}
+    </GenresList>
+  );
 }
 
 export default Genres;
