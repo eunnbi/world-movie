b22777f6f23c6435a72dfd3ac157d3909678eac9
Eunbi Kang <jenabill@naver.com>
2022-06-16 20:16:20 +0900

    Refactor: 스타일 컴포넌트명 수정 및 Input 스타일 컴포넌트 분리

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,38 +1,33 @@
 import Heading from "./common/Heading";
 import styled from "styled-components";
 
-const StyledSearchForm = styled.section`
-    margin-bottom: 50px;
-    h1 {
-      color: #112a42;
-      margin-bottom: 25px;
-    }
-    form {
-      input {
-        border: none;
-        width: 100%;
-        max-width: 600px;
-        height: 45px;
-        border-radius: 5px;
-        padding-left: 10px;
-        font-size: 20px;
-      }
-    }
+const FormWrapper = styled.section`
+  margin-bottom: 50px;
+  h1 {
+    color: #112a42;
+    margin-bottom: 25px;
+  }
 `;
 
-function SearchForm({query, onSearch}){
-    return (
-        <StyledSearchForm>
-            <Heading level={1} size={40}>Search Movies</Heading>
-            <form>
-                <input 
-                    type="text" 
-                    value={query}
-                    onChange={onSearch}
-                />
-            </form>    
-        </StyledSearchForm>
-    )
+const Input = styled.input`
+  border: none;
+  width: 100%;
+  max-width: 600px;
+  height: 3rem;
+  border-radius: 5px;
+  padding-left: 10px;
+  font-size: 20px;
+`;
+
+function SearchForm({ query, onSearch }) {
+  return (
+    <FormWrapper>
+      <Heading>Search Movies</Heading>
+      <form>
+        <Input type="text" value={query} onChange={onSearch} />
+      </form>
+    </FormWrapper>
+  );
 }
 
 export default SearchForm;
