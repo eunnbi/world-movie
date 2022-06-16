ddf2783cdef895be4a7b2cd57ae5dbb53e8f92ee
Eunbi Kang <jenabill@naver.com>
2022-06-16 21:41:46 +0900

    Design: Detail Page 레이아웃 수정

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,53 +1,51 @@
 import { useParams } from "react-router-dom";
 import { useQueries } from "react-query";
 import Loading from "../components/common/Loading";
-import DetailsPoster from "../components/Details/DetailsPoster";
+import MoviePoster from "../components/Movie/MoviePoster";
 import DetailsInfo from "../components/Details/DetailsInfo";
 import ReviewSection from "../components/Review/ReviewSection";
 import { getDetails, getReviews } from "../lib/api";
 import { useScroll } from "../hooks";
 import styled from "styled-components";
 
-
-
-
 const DetailMain = styled.main`
-    display: grid;
-    grid-template-columns: 1fr 2fr;
-    gap: 4rem 2rem;
-    width: 90vw;
-    max-width: 1170px;
-    margin: 4rem auto;
+  margin-top: 0;
+`;
 
-    @media screen and (max-width: 749px){
-        display: flex;
-        flex-direction: column;
-        gap: 0;
-    }
+const DetailRow = styled.div`
+  display: flex;
+  gap: 2rem;
+  margin-bottom: 6rem;
+  @media ${({ theme }) => theme.device.tablet} {
+    flex-direction: column;
+  }
 `;
 
 const Details = () => {
-    const { id } = useParams();
-    const [{isLoading : detailsLoading, data : details}, { isLoading : reviewsLoading, data: reviews}] = useQueries([
-        { queryKey: ['details', id], queryFn: getDetails},
-        { queryKey: ["reviews", id], queryFn: getReviews}
-    ])
-    const loading = detailsLoading || reviewsLoading;
-    useScroll([]);
+  const { id } = useParams();
+  const [
+    { isLoading: detailsLoading, data: details },
+    { isLoading: reviewsLoading, data: reviews },
+  ] = useQueries([
+    { queryKey: ["details", id], queryFn: getDetails },
+    { queryKey: ["reviews", id], queryFn: getReviews },
+  ]);
+  const loading = detailsLoading || reviewsLoading;
+  useScroll([]);
 
-    if (loading){
-        return (
-            <Loading/>
-        );
-    }
+  if (loading) {
+    return <Loading />;
+  }
 
-    return (
-        <DetailMain className="details__main">
-            <DetailsPoster poster={details.poster_path}/>
-            <DetailsInfo details={details}/>
-            <ReviewSection reviews={reviews}/> 
-        </DetailMain>
-    );
-}
+  return (
+    <DetailMain>
+      <DetailRow>
+        <MoviePoster poster={details.poster_path} />
+        <DetailsInfo details={details} />
+      </DetailRow>
+      <ReviewSection reviews={reviews} />
+    </DetailMain>
+  );
+};
 
 export default Details;
