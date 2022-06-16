326f7b73e400c89fd235eda05411f44089a96dee
Eunbi Kang <jenabill@naver.com>
2022-06-16 12:54:51 +0900

    Design: Home 페이지 레이아웃 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,43 +6,42 @@ import { moviesContext } from "../contexts/movies";
 import { useCloseNav, useScroll } from "../hooks";
 import styled from "styled-components";
 
-
 const HomeMain = styled.main`
-    padding: 100px 20px 0;
-    width: 85vw;
-    max-width: 1200px;
-    margin: 110px auto 0;
-    @media screen and (max-width: 1000px){
-        padding: 100px 0 0;
-    }
+  padding: 5rem 2rem 0;
+  width: 85vw;
+  max-width: 1200px;
+  margin: 110px auto 0;
+  @media screen and (max-width: 1000px) {
+    padding: 100px 0 0;
+  }
 `;
 
 const HomeSection = styled.section`
-    margin-bottom: 80px;
+  margin-bottom: 80px;
 `;
 
 const Home = () => {
-    useCloseNav();
-    useScroll([]);
-    const {loading, movies} = useContext(moviesContext);
-    if (loading){
-        return (
-            <Loading/>
-        );
-    }
+  useCloseNav();
+  useScroll([]);
+  const { loading, movies } = useContext(moviesContext);
+  if (loading) {
+    return <Loading />;
+  }
 
-    return (
-        <HomeMain className="home__main">
-            {movies &&
-                Object.entries(movies).map((entrie, index) => 
-                    <HomeSection key={index}>
-                        <SectionTitle title={entrie[0].replace("_", " ")} path={`/movies/${entrie[0]}`}/>
-                        <HomeMovieList movies={entrie[1].slice(0, 20)}/>
-                    </HomeSection>
-                )
-            }
-        </HomeMain>
-    );
-}
+  return (
+    <HomeMain className="home__main">
+      {movies &&
+        Object.entries(movies).map((entrie, index) => (
+          <HomeSection key={index}>
+            <SectionTitle
+              title={entrie[0].replace("_", " ")}
+              path={`/movies/${entrie[0]}`}
+            />
+            <HomeMovieList movies={entrie[1].slice(0, 20)} />
+          </HomeSection>
+        ))}
+    </HomeMain>
+  );
+};
 
 export default Home;
