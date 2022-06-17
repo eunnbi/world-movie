9aeabfac2dc8619c4068cacf68e3bdc83ed70573
Eunbi Kang <jenabill@naver.com>
2022-06-17 11:13:46 +0900

    Design: 불필요한 스타일 요소 제거 및 px에서 rem으로 단위 변경

--- src/components/Home/HomeMovieList.js
+++ src/components/Home/HomeMovieList.js
@@ -5,25 +5,22 @@ const StyledHomeMoviesList = styled.ul`
   display: flex;
   gap: 1rem;
   overflow-x: scroll;
-  padding-bottom: 30px;
+  padding-bottom: 1.5rem;
   &::-webkit-scrollbar {
     width: 6px;
-    height: 10px;
-    margin-top: 10px;
+    height: 0.5rem;
     display: block;
   }
   &::-webkit-scrollbar-thumb {
     background-color: rgba(0, 0, 0, 0.3);
     border-radius: 5px;
     transition: all 0.3s ease-in-out;
-    margin-top: 10px;
     &:active {
       background-color: rgba(0, 0, 0, 0.8);
     }
   }
   &::-webkit-scrollbar-track {
     background-color: transparent;
-    margin-top: 10px;
   }
 `;
 
