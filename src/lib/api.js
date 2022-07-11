1c7e91d09c7ea46fe1a2882cb4f88cd8909bdaa5
Eunbi Kang <jenabill@naver.com>
2022-07-11 21:46:52 +0900

    Refactor: 영화 detail 정보 가져오는 api url에 비디오 쿼리 추가

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,25 +3,32 @@ import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
 
-
 export const getMovies = async (type, page) => {
-    const { data } = await axios.get(`${BASE_URL}/movie/${type}?api_key=${API_KEY}&language=en-US&page=${page}`);
-    return data.results;
-}
+  const { data } = await axios.get(
+    `${BASE_URL}/movie/${type}?api_key=${API_KEY}&language=en-US&page=${page}`
+  );
+  return data.results;
+};
 
 export const getDetails = async ({ queryKey }) => {
-    const { data } = await axios.get(`${BASE_URL}/movie/${queryKey[1]}?api_key=${API_KEY}&language=en-US`);
-    return data; 
-}
+  const { data } = await axios.get(
+    `${BASE_URL}/movie/${queryKey[1]}?api_key=${API_KEY}&language=en-US&append_to_response=videos`
+  );
+  return data;
+};
 
-export const  getReviews = async ({queryKey}) => {
-    const { data } = await axios.get(`${BASE_URL}/movie/${queryKey[1]}/reviews?api_key=${API_KEY}&language=en-US`);
-    return data.results;
-}
+export const getReviews = async ({ queryKey }) => {
+  const { data } = await axios.get(
+    `${BASE_URL}/movie/${queryKey[1]}/reviews?api_key=${API_KEY}&language=en-US`
+  );
+  return data.results;
+};
 
-export const searchMovies = async({queryKey}) => {
-    const query = queryKey[1];
-    if (query === "") return [];
-    const { data } = await axios.get(`${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${queryKey[1]}&page=1&include_adult=false`);
-    return data.results;
-}
+export const searchMovies = async ({ queryKey }) => {
+  const query = queryKey[1];
+  if (query === "") return [];
+  const { data } = await axios.get(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${queryKey[1]}&page=1&include_adult=false`
+  );
+  return data.results;
+};
