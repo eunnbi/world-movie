63e15b24927294bab1ad39e8ae9acaa647a0394f
Eunbi Kang <jenabill@naver.com>
2022-07-12 01:06:51 +0900

    Refactor: BackButton에서 navigate하는 이벤트 함수 변경

--- src/components/Details/BackButton.js
+++ src/components/Details/BackButton.js
@@ -1,22 +1,34 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useQueryParam } from "../../hooks/useQueryParam";
+import { FiChevronLeft } from "react-icons/fi";
 import styled from "styled-components";
 
 const Button = styled.button`
   color: #fff;
   background-color: ${({ theme }) => theme.color.skyBlue};
   text-transform: capitalize;
-  padding: 0.4rem 0.6rem;
+  padding: 0.3rem;
   border-radius: 5px;
-  margin-top: 2rem;
+  margin: 2rem 0;
   transition: all 0.3s ease-in-out;
-  &:hover {
-    background-color: ${({ theme }) => theme.color.blueBlack};
+  svg {
+    font-size: 1.3rem;
   }
 `;
 
 function BackButton() {
   const navigate = useNavigate();
-  return <Button onClick={() => navigate(-1)}>back to movies</Button>;
+  const from = useQueryParam("from");
+  const page = useQueryParam("page");
+  const query = useQueryParam("query");
+  const to = `${from}${
+    page ? `?page=${page}` : query ? `?query=${query}` : ""
+  }`;
+  return (
+    <Button onClick={() => navigate(to)}>
+      <FiChevronLeft />
+    </Button>
+  );
 }
 
 export default BackButton;
