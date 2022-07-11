ffa3b0922094ae0c17f02a7e0fec8fab1e28f70f
Eunbi Kang <jenabill@naver.com>
2022-07-12 01:13:58 +0900

    Refactor: Movies page

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,11 +1,11 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 import MoviesTitle from "../components/MoviesTitle";
 import MovieList from "../components/common/MovieList";
 import Pagination from "../components/Pagination";
 import { moviesContext } from "../contexts/movies";
-import { useScroll } from "../hooks";
 import styled from "styled-components";
+import { useQueryParam } from "../hooks/useQueryParam";
 
 const MoviesMain = styled.main`
   width: 90vw;
@@ -13,13 +13,18 @@ const MoviesMain = styled.main`
 `;
 
 const Movies = () => {
+  const queryParam = useQueryParam("page");
+  const navigate = useNavigate();
   const { type } = useParams();
   const { movies } = useContext(moviesContext);
   const data = movies[type];
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(Number(queryParam));
   const limit = 12;
   const offset = (page - 1) * limit;
-  useScroll([page]);
+
+  useEffect(() => {
+    navigate(`?page=${page}`);
+  }, [page]);
 
   return (
     <MoviesMain>
