ca61c5c2632d417bb5354aabf2ce581b48593d5c
Eunbi Kang <jenabill@naver.com>
2022-07-11 20:31:08 +0900

    Refactor: show vote average information in Detail page

--- src/components/Details/DetailsInfo.js
+++ src/components/Details/DetailsInfo.js
@@ -2,6 +2,7 @@ import Heading from "../common/Heading";
 import BackButton from "./BackButton";
 import Genres from "./Genres";
 import styled from "styled-components";
+import { AiFillStar } from "react-icons/ai";
 
 const DetailHeading = styled.div`
   margin-bottom: 1.5rem;
@@ -20,23 +21,37 @@ const Overview = styled.p`
   line-height: 2.3rem;
 `;
 
+const Rating = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  svg {
+    color: #f4c519;
+  }
+`;
+
 function DetailsInfo({ details }) {
   const {
     title,
     release_date,
-    original_title: originalTitle,
+    original_title,
     genres,
     overview,
+    vote_average,
   } = details;
   const year = parseInt(release_date);
   return (
     <article>
       <DetailHeading>
         <Heading>{title}</Heading>
-        {title !== originalTitle && (
-          <Heading level={3}>{originalTitle}</Heading>
+        {title !== original_title && (
+          <Heading level={3}>{original_title}</Heading>
         )}
         <p>{!isNaN(year) && year}</p>
+        <Rating>
+          <AiFillStar />
+          <span>{vote_average}</span>
+        </Rating>
       </DetailHeading>
       <Genres genres={genres} />
       <Overview>{overview}</Overview>
